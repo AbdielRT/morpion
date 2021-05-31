8ee9d4675e2b67278b0ba93f25dce226a660fe61
art_server <polito.abs@gmail.com>
2021-05-31 17:34:10 +0200

    full board issues

--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
 const casilla = document.getElementsByClassName('casilla');
 const compteur = new Array(9);
+const player = ['Orange','Bleu'];
 var tour = true;
 
 for ( i = 0 ; i < casilla.length ; i++ ){
 	casilla[i].onclick = function clickCasilla(event){
 		j = parseInt(event.target.id);
 		
-		// event.target.innerHTML = (tour)? 'x' : 'o';
-		// compteur[j] = (tour)? 'x' : 'o' ;
-		// event.target.style.background = (tour)? 'orangered' : "darkcyan" ;
 		if (tour) {
 			event.target.innerHTML = 'x';
-			compteur[j] = 'x';
+			compteur[j] = 'Orange';
 			event.target.style.background = 'orangered';
 		} else {
 			event.target.innerHTML = 'o';
-			compteur[j] = 'o' ;
+			compteur[j] = 'Bleu' ;
 			event.target.style.background = "darkcyan" ;
 		}
 		
 		if (compteur.includes(undefined)){
 			for(k = 0; k < 2; k++){ // loop over the players
-			if (	// check columns 
-				compteur[0]==player[k] && compteur[1]==player[k] && compteur[2]==player[k] || 
-				compteur[3]==player[k] && compteur[4]==player[k] && compteur[5]==player[k] ||
-				compteur[6]==player[k] && compteur[7]==player[k] && compteur[8]==player[k] ||
-					// check rows 
-				compteur[0]==player[k] && compteur[3]==player[k] && compteur[6]==player[k] ||
-				compteur[1]==player[k] && compteur[4]==player[k] && compteur[7]==player[k] ||
-				compteur[2]==player[k] && compteur[5]==player[k] && compteur[8]==player[k] ||
-					// check diagonals
-				compteur[0]==player[k] && compteur[4]==player[k] && compteur[8]==player[k] ||
-				compteur[6]==player[k] && compteur[4]==player[k] && compteur[2]==player[k]	
-				){ alert('Joueur ' + player[k] + ' a gagné !')
-				document.location.reload(); 
+				if (	// check columns 
+					compteur[0]==player[k] && compteur[1]==player[k] && compteur[2]==player[k] || 
+					compteur[3]==player[k] && compteur[4]==player[k] && compteur[5]==player[k] ||
+					compteur[6]==player[k] && compteur[7]==player[k] && compteur[8]==player[k] ||
+						// check rows 
+					compteur[0]==player[k] && compteur[3]==player[k] && compteur[6]==player[k] ||
+					compteur[1]==player[k] && compteur[4]==player[k] && compteur[7]==player[k] ||
+					compteur[2]==player[k] && compteur[5]==player[k] && compteur[8]==player[k] ||
+						// check diagonals
+					compteur[0]==player[k] && compteur[4]==player[k] && compteur[8]==player[k] ||
+					compteur[6]==player[k] && compteur[4]==player[k] && compteur[2]==player[k]	
+					){ alert('Joueur ' + player[k] + ' a gagné !')
+					document.location.reload();
+				} 
 			} 
-		else {
-			alert('Essaie encore !'); 
-			document.location.reload();
+		}else {
+				alert('Essaie encore !'); 
+				document.location.reload();
 		}
-		tour = !tour;
-		}
+	tour = !tour;
 	}
+}
 
--- js/script3.js
+++ js/script3.js
@@ -1,43 +1,44 @@
 var casilla = document.getElementsByClassName('casilla');
 var compteur = new Array(9);
 var tour = true;
-var player = ['Orange','Blue'];
+var player = ['Orange','Bleu'];
 
-for ( i = 0 ; i < casilla.length ; i++ ){
+for (i = 0 ; i < casilla.length ; i++ ){
 	casilla[i].onclick = function clickCasilla(event){
 		j = parseInt(event.target.id);
 		if (!compteur[j]){	// prevent same box played twice
 			event.target.innerHTML = (tour)? 'x' : 'o';
-			compteur[j] = (tour)? 'Orange' : 'Blue' ;
+			compteur[j] = (tour)? 'Orange' : 'Bleu' ;
 			event.target.style.background = (tour)? 'orangered' : "darkcyan" ;
 			
-			checkWin();
+			if (compteur.includes(undefined) || i == casilla.length ){ 
+				for(k = 0; k < 2; k++){ // loop over the players
+					if (	// check columns 
+						compteur[0]==player[k] && compteur[1]==player[k] && compteur[2]==player[k] || 
+						compteur[3]==player[k] && compteur[4]==player[k] && compteur[5]==player[k] ||
+						compteur[6]==player[k] && compteur[7]==player[k] && compteur[8]==player[k] ||
+							// check rows 
+						compteur[0]==player[k] && compteur[3]==player[k] && compteur[6]==player[k] ||
+						compteur[1]==player[k] && compteur[4]==player[k] && compteur[7]==player[k] ||
+						compteur[2]==player[k] && compteur[5]==player[k] && compteur[8]==player[k] ||
+							// check diagonals
+						compteur[0]==player[k] && compteur[4]==player[k] && compteur[8]==player[k] ||
+						compteur[6]==player[k] && compteur[4]==player[k] && compteur[2]==player[k]	
+						){ alert('Joueur ' + player[k] + ' a gagné !')
+						document.location.reload(); 
+					} 
+				}
+			 }
+			//checkWinner();
 			tour = !tour;
-		} else if (i>=8) {
+		} else if (i >= casilla.length ) {
 		alert('Essaie encore !'); 
 		document.location.reload();}
 	}
 }
 
-function checkWin() {
-	if (compteur.includes(undefined) || i == casilla.length ){ 
-		for(k = 0; k < 2; k++){ // loop over the players
-			if (	// check columns 
-				compteur[0]==player[k] && compteur[1]==player[k] && compteur[2]==player[k] || 
-				compteur[3]==player[k] && compteur[4]==player[k] && compteur[5]==player[k] ||
-				compteur[6]==player[k] && compteur[7]==player[k] && compteur[8]==player[k] ||
-					// check rows 
-				compteur[0]==player[k] && compteur[3]==player[k] && compteur[6]==player[k] ||
-				compteur[1]==player[k] && compteur[4]==player[k] && compteur[7]==player[k] ||
-				compteur[2]==player[k] && compteur[5]==player[k] && compteur[8]==player[k] ||
-					// check diagonals
-				compteur[0]==player[k] && compteur[4]==player[k] && compteur[8]==player[k] ||
-				compteur[6]==player[k] && compteur[4]==player[k] && compteur[2]==player[k]	
-				){ alert('Joueur ' + player[k] + ' a gagné !')
-				document.location.reload(); 
-			} 
-		}
-	} else {
-		alert('Essaie encore !'); 
-		document.location.reload();}
-}
+//function checkWinner() {
+	 // else {
+	// 	alert('Essaie encore !'); 
+	// 	document.location.reload();}
+//}
