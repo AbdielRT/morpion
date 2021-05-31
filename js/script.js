c3e1e351c740047e19adcdce0c286d065195daea
art_server <polito.abs@gmail.com>
2021-05-31 15:29:30 +0200

    explicit if's

--- js/script.js
+++ js/script.js
@@ -1,45 +1,40 @@
-var casilla = document.getElementsByClassName('casilla');
-var compteur = new Array(9);
+const casilla = document.getElementsByClassName('casilla');
+const compteur = new Array(9);
 var tour = true;
 
 for ( i = 0 ; i < casilla.length ; i++ ){
 	casilla[i].onclick = function clickCasilla(event){
 		j = parseInt(event.target.id);
-
-		event.target.innerHTML = (tour)? 'x' : 'o';
-		compteur[j] = (tour)? 'x' : 'o' ;
-		event.target.style.background = (tour)? 'orangered' : "darkcyan" ;
+		
+		// event.target.innerHTML = (tour)? 'x' : 'o';
+		// compteur[j] = (tour)? 'x' : 'o' ;
+		// event.target.style.background = (tour)? 'orangered' : "darkcyan" ;
+		if (tour) {
+			event.target.innerHTML = 'x';
+			compteur[j] = 'x';
+			event.target.style.background = 'orangered';
+		} else {
+			event.target.innerHTML = 'o';
+			compteur[j] = 'o' ;
+			event.target.style.background = "darkcyan" ;
+		}
 		
 		if (compteur.includes(undefined)){
-			// Check if player 'x' wins
-			if ( 
-				compteur[0]=== 'x' && compteur[1]=== 'x' && compteur[2]=== 'x' || 		//check
-				compteur[3]=== 'x' && compteur[4]=== 'x' && compteur[5]=== 'x' ||		// for
-				compteur[6]=== 'x' && compteur[7]=== 'x' && compteur[8]=== 'x' ||		// rows
-
-				compteur[0]=== 'x' && compteur[3]=== 'x' && compteur[6]=== 'x' ||		//check
-				compteur[1]=== 'x' && compteur[4]=== 'x' && compteur[7]=== 'x' ||		// for
-				compteur[2]=== 'x' && compteur[5]=== 'x' && compteur[8]=== 'x' ||		// columns
-
-				compteur[0]=== 'x' && compteur[4]=== 'x' && compteur[8]=== 'x' ||		//check
-				compteur[6]=== 'x' && compteur[4]=== 'x' && compteur[2]=== 'x'	 		//diagonals
-				){ alert('Joueur Orange a gagné !')
-				document.location.reload();}
-			else if (
-			// Check if player 'o' wins
-				compteur[0]=== 'o' && compteur[1]=== 'o' && compteur[2]=== 'o' || 		//check
-				compteur[3]=== 'o' && compteur[4]=== 'o' && compteur[5]=== 'o' ||		// for
-				compteur[6]=== 'o' && compteur[7]=== 'o' && compteur[8]=== 'o' ||		// rows
-
-				compteur[0]=== 'o' && compteur[3]=== 'o' && compteur[6]=== 'o' ||		//check
-				compteur[1]=== 'o' && compteur[4]=== 'o' && compteur[7]=== 'o' ||		// for
-				compteur[2]=== 'o' && compteur[5]=== 'o' && compteur[8]=== 'o' ||		// columns
-
-				compteur[0]=== 'o' && compteur[4]=== 'o' && compteur[8]=== 'o' ||		//check
-				compteur[6]=== 'o' && compteur[4]=== 'o' && compteur[2]=== 'o'	 		//diagonals
-			){ alert('Joueur Blue a gagné !')
-				document.location.reload();}
-		}
+			for(k = 0; k < 2; k++){ // loop over the players
+			if (	// check columns 
+				compteur[0]==player[k] && compteur[1]==player[k] && compteur[2]==player[k] || 
+				compteur[3]==player[k] && compteur[4]==player[k] && compteur[5]==player[k] ||
+				compteur[6]==player[k] && compteur[7]==player[k] && compteur[8]==player[k] ||
+					// check rows 
+				compteur[0]==player[k] && compteur[3]==player[k] && compteur[6]==player[k] ||
+				compteur[1]==player[k] && compteur[4]==player[k] && compteur[7]==player[k] ||
+				compteur[2]==player[k] && compteur[5]==player[k] && compteur[8]==player[k] ||
+					// check diagonals
+				compteur[0]==player[k] && compteur[4]==player[k] && compteur[8]==player[k] ||
+				compteur[6]==player[k] && compteur[4]==player[k] && compteur[2]==player[k]	
+				){ alert('Joueur ' + player[k] + ' a gagné !')
+				document.location.reload(); 
+			} 
 		else {
 			alert('Essaie encore !'); 
 			document.location.reload();
